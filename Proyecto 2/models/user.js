d3685810dfff4241830666996b3821509a0be3b9
MauroMauro <blur.benitez@gmail.com>
2023-10-07 17:12:03 -0300

    implemented user model

---
+++ Proyecto 2/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        minLenght: 3,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        minLenght: 3
+    }
+})
+
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        // the passwordHash should not be revealed
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+userSchema.plugin(uniqueValidator)
+
+
+module.exports = User
