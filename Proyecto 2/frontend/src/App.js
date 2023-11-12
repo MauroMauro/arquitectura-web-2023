7568a4da1001e38b81def9e971b391a2fd80197d
MauroMauro <blur.benitez@gmail.com>
2023-11-12 12:08:51 -0300

    implemented login

--- Proyecto 2/frontend/src/App.js
+++ Proyecto 2/frontend/src/App.js
@@ -7,6 +7,10 @@ import {
 import Home from './components/Home'
 import Notifications from './components/Notification'
 import BloqueProductos from './components/BloqueProductos'
+import LoginForm from './components/LoginForm'
+
+import productoService from './services/producto'
+import loginService from './services/login'
 
 // Estilos
 import Button from 'react-bootstrap/Button'
@@ -17,26 +21,94 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 
-const App =() => {
+const App = () => {
   const [notification, setNotification] = useState(null)
   const [errorNotification, setErrorNotification] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedCustomerappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      productoService.setToken(user.token)
+    }
+  }, [])
+
+  const loginForm = () => (
+    <LoginForm
+      handleLogin={handleLogin}
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+    />
+  )
+
+  const logout = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedCustomerappUser', JSON.stringify(user)
+      )
+
+      productoService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorNotification('Wrong credentials')
+      setTimeout(() => {
+        setErrorNotification(null)
+      }, 5000)
+    }
+  }
 
   return (
-    <div>
+    <div className="container">
+      {user === null ?
+        loginForm() :
+        <div>
+          <Navbar expand="lg" className="bg-body-tertiarys">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className='me-auto'>
+              <Nav.Link href="http://localhost:3000">Home</Nav.Link>
+              <Nav.Link href="http://localhost:3000/productos">Productos</Nav.Link>
+              <Button onClick={() => logout()}>Cerrar sesión</Button>
+              </Nav>
+              
+            </Navbar.Collapse>
+          </Navbar>
 
-      <Nav.Link href="http://localhost:3000">Home</Nav.Link>
-      <Nav.Link href="http://localhost:3000/productos">Productos</Nav.Link>
-      <h1>Aplicación de productos - Arquitectura web</h1>
+          <h1>Aplicación de productos - Arquitectura web</h1>
 
-      <Notifications.Notification message={notification} />
-      <Notifications.ErrorNotification message={errorNotification} />
+          <Notifications.Notification message={notification} />
+          <Notifications.ErrorNotification message={errorNotification} />
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos" element={<BloqueProductos setNotification={setNotification} setErrorNotification={setErrorNotification} />} />
-        </Routes>
-      </Router>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/productos" element={<BloqueProductos setNotification={setNotification} setErrorNotification={setErrorNotification} />} />
+            </Routes>
+          </Router>
+        </div>
+      }
+
 
 
     </div>
--- Proyecto 2/frontend/src/services/producto.js
+++ Proyecto 2/frontend/src/services/producto.js
@@ -1,13 +1,47 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/products'
 
+let token = null
+
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+  }
+
 const getAll = () => {
     const request = axios.get(baseUrl)
-    console.log('==> request.then(response => response.data): ', request.then(response => response.data) )
-    
+
     return request.then(response => response.data)
 }
 
+
+const create = async newObject => {
+    const config = {
+        headers: { Authorization: token },
+    }
+
+    const response = await axios.post(baseUrl, newObject, config)
+    return response.data
+}
+
+const update = async (id, newObj) => {
+    const config = {
+        headers: { Authorization: token }
+    }
+
+    const response = await axios.put(baseUrl + '/' + id, newObj, config)
+    return response.data
+}
+
+const remove = async (id) => {
+    const config = {
+        headers: { Authorization: token }
+    }
+
+    const response = await axios.delete(baseUrl + '/' + id, config)
+    return response.data
+}
+
 export default {
-    getAll
+    getAll, create, update, setToken, remove
 }
